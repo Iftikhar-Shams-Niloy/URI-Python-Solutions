61e94e646433274995b0f8bbe7179f3369d4bcbc
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-06-03 11:40:14 -0700

    Add files via upload

---
+++ URI 1037.py
@@ -0,0 +1,11 @@
+num = float(input())
+if num > 0 and num<=25.0000000:
+    print("Intervalo [0,25]")
+elif num >= 25.00001 and num <=50.0000000 :
+    print("Intervalo (25,50]")
+elif num >= 50.00001 and num <=75.0000000 :
+    print("Intervalo (50,75]")
+elif num >=75.00001 and num <= 100.0000000:
+    print("Intervalo (75,100]")
+else :
+    print("Fora de intervalo")
