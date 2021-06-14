1fc67bc6fb011080a21ab5af170ddf78edf778b1
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-06-14 00:52:50 -0700

    Add files via upload

---
+++ URI 1060.py
@@ -0,0 +1,21 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+num6 = float(input())
+flag = 0
+if num1 > 0 :
+    flag = flag+1
+if num2 > 0 :
+    flag = flag+1
+if num3 > 0:
+    flag = flag + 1
+if num4 > 0 :
+    flag = flag+1
+if num5 > 0 :
+    flag = flag+1
+if num6 > 0 :
+    flag = flag+1
+
+print("%d valores positivos"%flag)
