359725b99e2ffb8020fe4636e927cbf202f6cd00
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-06-21 15:44:35 -0700

    Add files via upload

---
+++ URI 1074.py
@@ -0,0 +1,15 @@
+N = int(input())
+for i in range(N):
+    num = int(input())
+    if num > 0 :
+        if num%2 == 0:
+            print("EVEN POSITIVE")
+        else:
+            print("ODD POSITIVE")
+    elif num < 0:
+        if num%2 == 0:
+            print("EVEN NEGATIVE")
+        else:
+            print("ODD NEGATIVE")
+    elif num == 0:
+        print("NULL")
